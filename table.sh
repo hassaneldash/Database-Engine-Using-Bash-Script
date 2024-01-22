8469880f61a14ee8e22a8852a550ef9b91c667b9
hassaneldash <hassan.eldash@yahoo.com>
2024-01-22 20:54:12 +0200

    Changing Validation

--- table.sh
+++ table.sh
@@ -1,64 +1,17 @@
 #!/usr/bin/bash
 
 PS3=$'---------------------------- \nSelect From Table Menu: '
-# current_db="$1"  # Save the current database in a variable
-
-# # Change directory to the script's directory
-# cd "$(dirname "${BASH_SOURCE[0]}")/.db/$current_db"
 cd ./.db/$1
 
 select var in "Create table" "List table" "Drop table" "Insert to table" "Select From table" "Delete From table" "Update From table" "Exit"
 do 
     case $var in 
         "Create table")
-            # read -p "Please, Enter table Name, Dr.Mina <3 : " name
-
-            # #check regex  
-            # name=$(echo "$name" | tr " " "_")
-            
-            # if [[ ! $name =~ ^[0-9] ]]; then
-    
-            #     #make name of table :
-            #     touch "${name}"
-
-            #     #make 3 columns and take the value 
-            #     #so 3 values : 
-
-            #     #first columns
-            #     read -p "Please, Enter first columns, Dr.Mina <3:" id
-            
-            #     #second columns 
-            #     read -p "Please, Enter Second Columns, Dr.Mina <3:" type
-            #     #third columns 
-            #     read -p "Please, Enter Third columns, Dr.Mina <3:" string
-
-            #     #make variable to save file name with .sh
-            #     source_file="${name}"
-
-            #     #we must give file mod u+x $source_file
-            #     chmod u+rwx "${name}"
-
-            #     #save in file that make with $name with path
-            #     echo "id=\"$id\"" >> "$source_file"
-            #     #now save in file that make #name with path
-            #     echo "type=\"$type\"" >> "$source_file"
-            #     #now save in file that make #name with path
-            #     echo "string=\"$string\"" >> "$source_file"
-
-            #     #echo file if created
-            #     echo "Table '$name' created successfully, Dr.Mina <3"
-            #     echo "--------------------------------------------------------"
-
-            # else
-            #     echo "Sorry , Dr.Mina <3 ; There is an error, Invalid name."
-            #     echo "--------------------------------------------------------"
-
-            # fi
 
             read -p "Please, Enter table Name, Dr.Mina <3 : " name
             if [[ $name == *['!'@#\$%^\&*()-+\.\/]* ]]; then
                 echo 
-                echo "! @ # $ % ^ () + . -  are not allowed!"
+                echo "! @ # $ % ^ () + . -  are not allowed, Dr.Mina <3 !"
                 continue
             fi
             name=`echo "$name" | tr " " "_"`
@@ -85,13 +38,6 @@ do
                     echo -n " $col : " >> $name.meta 
                     echo -n " $col : " >> $name 
                     done
-
-                # # Add primary key option
-                #     read -p "Please, Enter Primary Key Name, Dr.Mina <3: " pk_name
-                #     read -p "Please, Enter Primary Key Type (Integer/String), Dr.Mina <3: " pk_type
-                #     echo "Primary Key: $pk_name, Type: $pk_type" >> $name.meta
-                #     echo "pk_name=\"$pk_name\"" >> "$name"
-                #     echo "pk_type=\"$pk_type\"" >> "$name"
 
                     echo "" >> $name.meta 
                     echo "" >> $name 
@@ -154,43 +100,34 @@ do
             source_file="${name}"
             if [[ -f $name ]]; then
 
-                # Here we will make a function to check unique id
-                function check_unique_id {
-                    local check_id="$id"
-                    grep -q "id=\"$check_id\"" "${name}"
-                }
-
                 # Here I will make a while loop until entering a unique id
                 # make variable to save file name with .sh
-                while true;
-                do
+                while true; do
                     # first columns
                     read -p "Please, Enter first columns value As PK, Dr.Mina <3: " id
-                    
 
-                    if check_unique_id "$id"; then
-                        echo "Sorry , Dr.Mina <3 ; ID is not unique. Please enter a unique ID."
-                    else
+                    is_unique=true
+
+                    # Check uniqueness of ID
+                    for field in $(cut -f1 -d: "./$dbname/$name"); do
+                        if [[ $field = "$id" ]]; then
+                            echo "Sorry, Dr.Mina <3; ID is not unique. Please enter a unique ID."
+                            is_unique=false
+                            break
+                        fi
+                    done
+
+                    if [[ $is_unique == true ]]; then
+                        echo -n "$id : " >> "$source_file"
+                        for ((i = 2; i <= num_columns; i++)); do
+                            read -p "Please, Enter Data for Column $i, Dr.Mina <3: " data
+                            echo -n "$data : " >> "$source_file"
+                        done
+                        echo " " >> "$source_file"
+                        echo "Values inserted successfully, Dr.Mina <3."
                         break
-                    fi 
+                    fi
                 done
-            
-                # # second columns 
-                # read -p "Please, Enter Second Columns value, Dr.Mina <3:" type
-                # # third columns 
-                # read -p "Please, Enter Third columns value, Dr.Mina <3:" string 
-                # # save in file that make with $name with path
-                # echo "value=\"$id\"" >> "$source_file"
-                # # now save in file that make #name with path
-                # echo "value=\"$type\"" >> "$source_file"
-                # # now save in file that make #name with path
-                # echo "value=\"$string\"" >> "$source_file"
-                echo -n "$id : " >> "$source_file"
-                for ((i = 2; i <= num_columns; i++)); do
-                    read -p "Please, Enter Data for Column $i , Dr.Mina <3: " data
-                    echo -n "$data : " >> "$source_file"
-                done
-
                 echo "Values inserted successfully, Dr.Mina <3."
                 
             else
